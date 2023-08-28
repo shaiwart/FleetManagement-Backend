48096883be743376b7edf9fef979300a29aef8dc
shaiwart <133580233+shaiwart@users.noreply.github.com>
2023-08-28 13:23:39 +0530

    changed userRepository query
    Instead of select *
    user_id, aadhar_no, address, dl_no, email_id, first_name, last_name, mobile_number, passport_no, city_id, state_id

--- src/main/java/com/example/controllers/CarCategoryController.java
+++ src/main/java/com/example/controllers/CarCategoryController.java
@@ -3,6 +3,7 @@ package com.example.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import com.example.entities.CarCategory;
 import com.example.services.CarCategoryService;
 
 @RestController
+@CrossOrigin
 public class CarCategoryController {
 	
 	@Autowired
